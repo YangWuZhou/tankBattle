eb162017f6049a39df4f30e520d2d8f3a85fde06
unknown <1393733592@qq.com>
2019-04-24 16:32:06 +0800

    删除了Numbers类

--- materiallogindemo-master/app/src/main/java/com/sourcey/footBook/utils/Numbers.java
+++
@@ -1,9 +0,0 @@
-package com.sourcey.footBook.utils;
-
-public class Numbers {
-    public static final int ZERO = 0;
-    public static final int FOUR = 4;
-    public static final int EIGHT = 8;
-    public static final int TWELVE = 12;
-    public static final int SIXTEEN = 16;
-}
